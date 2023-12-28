8853e46eb776cc6a1ede4f886f9c08ca672be4b3
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2023-12-27 23:45:56 -0800

    Start work on guest parsing and database seeding

---
+++ src/lib/parseCsv.ts
@@ -0,0 +1,110 @@
+import fs from "fs";
+import { parse } from "csv-parse";
+
+export type Guest = {
+  lastName: string;
+  adultFirstNames: string[];
+  childrenFirstNames: string[];
+  adults: number;
+  children: number;
+  plusOne: boolean;
+  fullNames: string[];
+  userName: string;
+};
+
+function parseFirstNames(
+  firstName: string,
+  lastName: string
+): { fullNames: string[]; adults: string[]; children: string[] } {
+  const adults: string[] = [];
+  const children: string[] = [];
+
+  const adultNames = firstName.split(",")[0].trim();
+  const childNames = firstName.split(",")[1]?.trim();
+
+  //   console.log(`adultsNames: ${adultNames}, childrenNames: ${childNames}`);
+
+  const fullNames: string[] = [];
+
+  if (adultNames) {
+    const adultList = adultNames.split("&").map((name) => name.trim());
+    for (const adult of adultList) {
+      if (adult.split(" ").length > 1) {
+        // custom last name
+        const [first, ...last] = adult.split(" ");
+        fullNames.push(`${first} ${last.join(" ")}`);
+      } else {
+        fullNames.push(`${adult} ${lastName}`);
+      }
+    }
+    adults.push(...adultList);
+  }
+
+  if (childNames) {
+    const childList = childNames.split("&").map((name) => name.trim());
+    for (const child of childList) {
+      fullNames.push(`${child} ${lastName}`);
+    }
+    children.push(...childList);
+  }
+
+  return { fullNames, adults, children };
+}
+
+const parseCsv = async () => {
+  const filePath = "./guests.csv";
+  const csvData = await fs.promises.readFile(filePath, "utf-8");
+  const records = parse(csvData, {
+    columns: true,
+    bom: true,
+    trim: true,
+    skipEmptyLines: true,
+  });
+
+  const arr = await records.toArray();
+
+  const guests = arr.map((guest) => {
+    const adultNumber = parseInt(guest["Adults"]);
+    const childrenNumber = parseInt(guest["Children"]);
+    const plusOne = Boolean(guest["Plus One?"] === "Y");
+    const { adults, children, fullNames } = parseFirstNames(
+      guest["First Name(s)"],
+      guest["Last Name"]
+    );
+    return {
+      lastName: guest["Last Name"],
+      adultFirstNames: adults,
+      childrenFirstNames: children,
+      adults: adultNumber ? adultNumber : 0,
+      children: childrenNumber ? childrenNumber : 0,
+      plusOne,
+      fullNames,
+      userName: guest["User Name"],
+    };
+  });
+
+  let oopsCount = 0;
+  for (const guest of guests) {
+    let adultMismatch = false;
+    let childrenMismatch = false;
+    if (guest.adultFirstNames.length + Number(guest.plusOne) !== guest.adults) {
+      adultMismatch = true;
+    }
+    if (guest.childrenFirstNames.length !== guest.children) {
+      childrenMismatch = true;
+    }
+    if (adultMismatch || childrenMismatch) {
+      oopsCount++;
+      console.log("_-_-_-_-");
+      if (childrenMismatch) console.log("children mismatch");
+      if (adultMismatch) console.log("adults mismatch");
+      console.log(guest);
+      console.log("_-_-_-_-");
+    }
+  }
+  //   console.log(`total: ${guests.length}, oops: ${oopsCount}`);
+  return guests;
+};
+
+const guests = await parseCsv();
+fs.writeFileSync("./guests.json", JSON.stringify(guests, null, 2));
