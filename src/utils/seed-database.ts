c29599c1b37ad8ca138dd6e8836654ebab2ea8ce
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2024-02-01 22:43:38 -0800

    Comment out unused code

--- src/utils/seed-database.ts
+++ src/utils/seed-database.ts
@@ -1,39 +1,39 @@
-import { createClient } from "@supabase/supabase-js";
-import { getAllUserAccounts } from "./seed-supabase-auth";
+// import { createClient } from "@supabase/supabase-js";
+// import { getAllUserAccounts } from "./seed-supabase-auth";
 
-let users;
+// let users;
 
-const supabaseUrl = Bun.env.SUPABASE_URL;
-const supabaseKey = Bun.env.SUPABASE_ANON_KEY;
+// const supabaseUrl = Bun.env.SUPABASE_URL;
+// const supabaseKey = Bun.env.SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("No SUPABASE_URL and SUPABASE_ANON_KEY found in.env file");
-} else {
-  users = await import("./users.json");
-}
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error("No SUPABASE_URL and SUPABASE_ANON_KEY found in.env file");
+// } else {
+//   users = await import("./users.json");
+// }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-const authUsers = (await getAllUserAccounts()).users;
-// @ts-ignore
-const userData = users.default;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+// const authUsers = (await getAllUserAccounts()).users;
+// // @ts-ignore
+// const userData = users.default;
 
-const seedDatabase = async () => {
-  for (const user of authUsers) {
-    const [username, _] = user?.email?.split("@") || "";
-    const userDataEntry = userData.find(
-      (entry: any) => entry.userName === username
-    );
+// const seedDatabase = async () => {
+//   for (const user of authUsers) {
+//     const [username, _] = user?.email?.split("@") || "";
+//     const userDataEntry = userData.find(
+//       (entry: any) => entry.userName === username
+//     );
 
-    console.log(userDataEntry);
+//     console.log(userDataEntry);
 
-    await supabase
-      .from("groups")
-      .upsert({
-        id: user.id,
-        rsvp: JSON.stringify(userDataEntry),
-      })
-      .select();
-  }
-};
+//     await supabase
+//       .from("groups")
+//       .upsert({
+//         id: user.id,
+//         rsvp: JSON.stringify(userDataEntry),
+//       })
+//       .select();
+//   }
+// };
 
-seedDatabase();
+// seedDatabase();
--- src/utils/seed-supabase-auth.ts
+++ src/utils/seed-supabase-auth.ts
@@ -1,70 +1,70 @@
-import { createClient, type User } from "@supabase/supabase-js";
+// import { createClient, type User } from "@supabase/supabase-js";
 
-let guests;
-const supabaseUrl = Bun.env.SUPABASE_URL;
-const supabaseKey = Bun.env.SUPABASE_SERVICE_ROLE;
+// let guests;
+// const supabaseUrl = Bun.env.SUPABASE_URL;
+// const supabaseKey = Bun.env.SUPABASE_SERVICE_ROLE;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("No SUPABASE_URL and SUPABASE_ANON_KEY found in.env file");
-} else {
-  guests = await import("./guests.json");
-}
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error("No SUPABASE_URL and SUPABASE_ANON_KEY found in.env file");
+// } else {
+//   guests = await import("./guests.json");
+// }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function createAccount(email: string, password: string) {
-  try {
-    const { data, error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-    if (error) throw error;
-    return data;
-  } catch (error) {
-    console.error("Error creating account:", error);
-    return null;
-  }
-}
+// async function createAccount(email: string, password: string) {
+//   try {
+//     const { data, error } = await supabase.auth.signUp({
+//       email: email,
+//       password: password,
+//     });
+//     if (error) throw error;
+//     return data;
+//   } catch (error) {
+//     console.error("Error creating account:", error);
+//     return null;
+//   }
+// }
 
-async function makeGuestAccounts(userNames: string[]) {
-  for (const userName of userNames) {
-    console.log("Creating user:", userName);
-    await createAccount(userName + "@rsvp.ing", "password123");
-  }
-}
+// async function makeGuestAccounts(userNames: string[]) {
+//   for (const userName of userNames) {
+//     console.log("Creating user:", userName);
+//     await createAccount(userName + "@rsvp.ing", "password123");
+//   }
+// }
 
-async function clearAllAccounts() {
-  const userAccounts = await getAllUserAccounts();
-  for (const user of userAccounts.users) {
-    console.log("Deleting user:", user.email);
-    await supabase.auth.admin.deleteUser(user.id);
-  }
-}
+// async function clearAllAccounts() {
+//   const userAccounts = await getAllUserAccounts();
+//   for (const user of userAccounts.users) {
+//     console.log("Deleting user:", user.email);
+//     await supabase.auth.admin.deleteUser(user.id);
+//   }
+// }
 
-export async function getAllUserAccounts() {
-  let nextPage = 1;
-  const userAccounts: { users: User[] } = { users: [] };
-  while (nextPage) {
-    const { data, error } = await supabase.auth.admin.listUsers({
-      page: nextPage,
-    });
-    if (error) {
-      console.error("Error getting all users:", error);
-    }
-    userAccounts.users.push(...data.users);
-    // @ts-ignore
-    if (data.nextPage) {
-      // @ts-ignore
-      nextPage = data.nextPage;
-    } else {
-      break;
-    }
-  }
-  console.log(userAccounts.users.length);
-  return userAccounts;
-}
+// export async function getAllUserAccounts() {
+//   let nextPage = 1;
+//   const userAccounts: { users: User[] } = { users: [] };
+//   while (nextPage) {
+//     const { data, error } = await supabase.auth.admin.listUsers({
+//       page: nextPage,
+//     });
+//     if (error) {
+//       console.error("Error getting all users:", error);
+//     }
+//     userAccounts.users.push(...data.users);
+//     // @ts-ignore
+//     if (data.nextPage) {
+//       // @ts-ignore
+//       nextPage = data.nextPage;
+//     } else {
+//       break;
+//     }
+//   }
+//   console.log(userAccounts.users.length);
+//   return userAccounts;
+// }
 
-await clearAllAccounts();
-// @ts-ignore
-await makeGuestAccounts(guests.default.map((guest) => guest.userName));
-await getAllUserAccounts();
+// await clearAllAccounts();
+// // @ts-ignore
+// await makeGuestAccounts(guests.default.map((guest) => guest.userName));
+// await getAllUserAccounts();
