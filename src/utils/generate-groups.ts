84fa01727cee3d51f51c21b2c6df60567bfe4704
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2024-04-19 22:22:43 -0700

    add randomized password support

--- src/utils/generate-groups.ts
+++ src/utils/generate-groups.ts
@@ -1,35 +1,35 @@
-// import * as guests from "./guests.json";
-// import { FoodChoice, type Group, type Person } from "../lib/types";
-// import type { Guest } from "./parse-csv";
+import * as guests from "./guests.json";
+import { FoodChoice, type Group, type Person } from "../lib/types";
+import type { Guest } from "./parse-csv";
 
-// // @ts-ignore
-// const typedGuests = guests.default as Guest[];
+// @ts-ignore
+const typedGuests = guests.default as Guest[];
 
-// const generateGroups = async () => {
-//   const groups: Group[] = [];
+const generateGroups = async () => {
+  const groups: Group[] = [];
 
-//   for (const guest of typedGuests) {
-//     const userName = guest.userName;
+  for (const guest of typedGuests) {
+    const userName = guest.userName;
 
-//     const people = guest.fullNames.map((fullName) => {
-//       const [firstName, lastName] = fullName.split(" ");
-//       return {
-//         going: true,
-//         firstName,
-//         lastName,
-//         foodChoice: FoodChoice.CHICKEN,
-//       } as Person;
-//     });
+    const people = guest.fullNames.map((fullName) => {
+      const [firstName, lastName] = fullName.split(" ");
+      return {
+        going: true,
+        firstName,
+        lastName,
+        foodChoice: FoodChoice.CHICKEN,
+      } as Person;
+    });
 
-//     const group = {
-//       userName,
-//       people: people,
-//       canHavePlusOne: guest.plusOne,
-//     } as Group;
+    const group = {
+      userName,
+      people: people,
+      canHavePlusOne: guest.plusOne,
+    } as Group;
 
-//     groups.push(group);
-//   }
-//   await Bun.write("users.json", JSON.stringify(groups));
-// };
+    groups.push(group);
+  }
+  await Bun.write("users.json", JSON.stringify(groups));
+};
 
-// await generateGroups();
+await generateGroups();
--- src/utils/seed-supabase-auth.ts
+++ src/utils/seed-supabase-auth.ts
@@ -1,70 +1,122 @@
-// import { createClient, type User } from "@supabase/supabase-js";
+import { createClient, type User } from "@supabase/supabase-js";
 
-// let guests;
-// const supabaseUrl = Bun.env.SUPABASE_URL;
-// const supabaseKey = Bun.env.SUPABASE_SERVICE_ROLE;
+let guests;
+const supabaseUrl = Bun.env.SUPABASE_URL;
+const supabaseKey = Bun.env.SUPABASE_SERVICE_ROLE;
 
-// if (!supabaseUrl || !supabaseKey) {
-//   throw new Error("No SUPABASE_URL and SUPABASE_ANON_KEY found in.env file");
-// } else {
-//   guests = await import("./guests.json");
-// }
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("No SUPABASE_URL and SUPABASE_ANON_KEY found in.env file");
+} else {
+  guests = await import("./guests.json");
+}
 
-// const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// async function createAccount(email: string, password: string) {
-//   try {
-//     const { data, error } = await supabase.auth.signUp({
-//       email: email,
-//       password: password,
-//     });
-//     if (error) throw error;
-//     return data;
-//   } catch (error) {
-//     console.error("Error creating account:", error);
-//     return null;
-//   }
-// }
+async function createAccount(email: string, password: string) {
+  try {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error("Error creating account:", error);
+    return null;
+  }
+}
 
-// async function makeGuestAccounts(userNames: string[]) {
-//   for (const userName of userNames) {
-//     console.log("Creating user:", userName);
-//     await createAccount(userName + "@rsvp.ing", "password123");
-//   }
-// }
+async function makeGuestAccounts(userNames: string[]) {
+  const accountDetails = [];
+  for (const userName of userNames) {
+    const password = generatePassword();
+    console.log(`Creating account for ${userName} with password: ${password}`);
+    try {
+      await createAccount(userName + "@rsvp.ing", password);
+      accountDetails.push({ email: userName + "@rsvp.ing", password });
+    } catch (error) {
+      console.error("Error creating account:", error);
+    }
+  }
+  await Bun.write("accountDetails.json", JSON.stringify(accountDetails));
+}
 
-// async function clearAllAccounts() {
-//   const userAccounts = await getAllUserAccounts();
-//   for (const user of userAccounts.users) {
-//     console.log("Deleting user:", user.email);
-//     await supabase.auth.admin.deleteUser(user.id);
-//   }
-// }
+const loveWords = [
+  "forever",
+  "unity",
+  "bliss",
+  "eternal",
+  "devotion",
+  "passion",
+  "romance",
+  "cherish",
+  "adore",
+  "heart",
+  "soul",
+  "vows",
+  "kiss",
+  "embrace",
+  "promise",
+  "dream",
+  "together",
+  "joy",
+  "happiness",
+  "love",
+  "bond",
+  "affection",
+  "spark",
+  "dove",
+  "harmony",
+  "loyalty",
+  "trust",
+  "family",
+  "companion",
+  "delight",
+  "grace",
+  "laughter",
+];
 
-// export async function getAllUserAccounts() {
-//   let nextPage = 1;
-//   const userAccounts: { users: User[] } = { users: [] };
-//   while (nextPage) {
-//     const { data, error } = await supabase.auth.admin.listUsers({
-//       page: nextPage,
-//     });
-//     if (error) {
-//       console.error("Error getting all users:", error);
-//     }
-//     userAccounts.users.push(...data.users);
-//     // @ts-ignore
-//     if (data.nextPage) {
-//       // @ts-ignore
-//       nextPage = data.nextPage;
-//     } else {
-//       break;
-//     }
-//   }
-//   console.log(userAccounts.users.length);
-//   return userAccounts;
-// }
+function generatePassword() {
+  const randomWords = new Set();
+  while (randomWords.size < 3) {
+    const randomIndex = Math.floor(Math.random() * loveWords.length);
+    randomWords.add(loveWords[randomIndex]);
+  }
+  return Array.from(randomWords).join("-");
+}
 
-// await clearAllAccounts();
-// // @ts-ignore
-// await makeGuestAccounts(guests.default.map((guest) => guest.userName));
-// await getAllUserAccounts();
+async function clearAllAccounts() {
+  const userAccounts = await getAllUserAccounts();
+  for (const user of userAccounts.users) {
+    console.log("Deleting user:", user.email);
+    await supabase.auth.admin.deleteUser(user.id);
+  }
+}
+
+export async function getAllUserAccounts() {
+  let nextPage = 1;
+  const userAccounts: { users: User[] } = { users: [] };
+  while (nextPage) {
+    const { data, error } = await supabase.auth.admin.listUsers({
+      page: nextPage,
+    });
+    if (error) {
+      console.error("Error getting all users:", error);
+    }
+    userAccounts.users.push(...data.users);
+    // @ts-ignore
+    if (data.nextPage) {
+      // @ts-ignore
+      nextPage = data.nextPage;
+    } else {
+      break;
+    }
+  }
+  console.log(userAccounts.users.length);
+  return userAccounts;
+}
+
+await clearAllAccounts();
+// @ts-ignore
+await makeGuestAccounts(guests.default.map((guest) => guest.userName));
+await getAllUserAccounts();
